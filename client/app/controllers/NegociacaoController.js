31a9b01d918289681865b20851066d90cb15976a
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2018-11-14 15:44:25 -0200

    Criação da View.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,12 +7,16 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event) {
 
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao())
+        this._negociacoesView.update(this._negociacoes);
         this._limpaFormulario();
     }
 
