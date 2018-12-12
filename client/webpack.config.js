8de4fd270113841b3bd5c4abd67d9f45eeeced13
Leandro A L Coelho <leandro.coelho@ibge.gov.br>
2018-12-12 11:13:26 -0200

    Otimizando o webpack para gerar bundle separado para as bibliotecas (vedor).

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,11 @@ plugins.push(new webpack.ProvidePlugin({
     jQuery: 'jquery/dist/jquery.js'
 }));
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
 if (process.env.NODE_ENV == 'production') {
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
@@ -32,7 +37,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
